e919534a9687f2fe6039e831b5df06a637f04350
ADHI-786 <adhithyan.1802005@srit.org>
2021-08-28 21:16:00 +0530

    class components

---
+++ src/components/repos/Repo.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import RepoItem from "./RepoItem";
+
+
+const Repos = ({ repos }) => {
+    return (
+        repos.map(repo => <RepoItem key={repo.id} repo={repo} /> )
+    );
+};
+
+
+Repos.propTypes = {
+    repos:PropTypes.array.isRequired,
+}
+
+export default Repos;
